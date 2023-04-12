787db35a51e1f3cd4c14e2a4a2fb79011393a025
Pratik <pratiktiwari3792@gmail.com>
2023-04-13 02:00:07 +0530

    Finished data modelling in prisma schema

--- utils/db_utils/client.ts
+++ utils/db_utils/client.ts
@@ -1,3 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({ log: ["query", "error", "info"] });
+// export const prisma = new PrismaClient({ log: ["query", "error", "info"] });
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+// let prisma
+
+if (process.env.NEXT_ENV === "production") {
+  prisma = new PrismaClient({ log: ["query", "error", "info"] });
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({ log: ["query", "error", "info"] });
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
