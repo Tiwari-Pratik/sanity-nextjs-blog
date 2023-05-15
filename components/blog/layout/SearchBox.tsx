fd3cd2ec35ab40db56de7bca74ae0749edc51536
pratik <pratiktiwari3792@gmail.com>
2023-05-15 17:14:29 +0530

    experimenting with UI

--- components/blog/layout/SearchBox.tsx
+++ components/blog/layout/SearchBox.tsx
@@ -4,7 +4,11 @@ import styles from "./SearchBox.module.css";
 const SearchBox = () => {
   return (
     <div className={styles.searchDiv}>
-      <input type="search" className={styles.search} placeholder="Search..." />
+      <input
+        type="search"
+        className={styles.search}
+        placeholder="Search for..."
+      />
       <MagnifyingGlassIcon className={styles.searchIcon} />
     </div>
   );
